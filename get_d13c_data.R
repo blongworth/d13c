1c3860f8f4fc3f53fb0f49b990d940e2c073e585
Brett Longworth <blongworth@gmail.com>
2020-04-29 18:01:30 -0400

    Moderately successful db pull for most process types

--- get_d13c_data.R
+++ get_d13c_data.R
@@ -3,15 +3,50 @@
 library(tidyverse)
 library(odbc)
 library(amstools)
+library(here)
 
-con <- conNOSAMS()
 
-dbListFields(con, "gas_sample")
+# Use amstools to get the standards table
+standards <- getStdTable()
 
-dbGetQuery(con, "select * from no_os where tp_num > 250000")
-dbGetQuery(con, "select * from mass_spec where ms_date > '2020-04-01'")
-dbGetQuery(con, "select * from gas_sample where gs_date > '2020-04-01'")
+# a function to grab data for a list of rec_nums from the gs table
+get13c <- function(type, recnums) {
+  table <- switch (type,
+                   "oc" = "organic_carbon",
+                   "hy" = "inorganic_carbon",
+                   "ws" = "water_strip",
+                   "gs" = "gas_sample"
+  )
+  
+  db <- conNOSAMS()
+  if (type %in% c("hy", "gs")) {
+    query <- glue_sql(paste(glue("SELECT {type}_num, rec_num, {type}_date AS date, 
+                                 {type}_rd AS rd, {type}_dc_13 AS d13c, {type}_dc13_src AS irms"),
+                            "FROM {`table`}
+                            WHERE rec_num IN ({recnums*})",
+                            glue("AND {type}_dc_13 IS NOT NULL")),
+                      .con = db)
+  } else if (type == "oc") {
+    query <- glue_sql(paste(glue("SELECT {type}_num, rec_num, date_run AS date, 
+                                 oc_devel AS rd, {type}_dc_13 AS d13c, {type}_dc13_src AS irms"),
+                            "FROM {`table`}
+                            WHERE rec_num IN ({recnums*})",
+                            glue("AND {type}_dc_13 IS NOT NULL")),
+                      .con = db)
+  } else if (type == "ws") {
+    query <- glue_sql(paste(glue("SELECT {type}_num, rec_num, {type}_strip_date AS date, 
+                                 {type}_r_d AS rd, {type}_delta_c13 AS d13c, {type}_dc13_from AS irms"),
+                            "FROM {`table`}
+                            WHERE rec_num IN ({recnums*})",
+                            glue("AND {type}_delta_c13 IS NOT NULL")),
+                      .con = db)
+  }
+  recs <- odbc::dbSendQuery(db, query)
+  data <- odbc::dbFetch(recs)
+  odbc::dbClearResult(recs)
+  data
+}
 
-# dc13 and source are stored in process tables. Ick.
+data <- map_dfr(list("hy", "gs", "oc", "ws"), get13c, standards$rec_num)
 
-dbListTables(con)
+write_csv(data, here("data/NOSAMS_d13c.csv"))
