ade131c51a38a5279cb09d7b3192951f0596886f
Brett Longworth <blongworth@gmail.com>
2020-05-27 16:57:20 -0400

    function to parse one file.

--- parse_irms_files.R
+++ parse_irms_files.R
@@ -9,6 +9,7 @@ top_dir_path <- "H://arg/"
 files <- dir(path = top_dir_path, full.names = TRUE, recursive = FALSE, pattern = "[optima|PRISM]\\d{2}.xlsx")
 str(files)
 
+# read and parse a single file
 col_names <- c("date", "time", "index", "name", "pres", "ref", "raw13c", 
                "raw18o", "err13c", "err18o", "blank", "pdbcorr13c", 
                "pdbcorr18o", "craigcor13c", "craigcor18o")
@@ -19,4 +20,21 @@ datafilt <- data %>%
   mutate(date = excel_numeric_to_date(as.numeric(date)),
          time = format(as.POSIXct(Sys.Date() + as.numeric(time)), "%H:%M", tz="UTC"))
 
+readIRMSfile <- function(file) {
+col_names <- c("date", "time", "index", "name", "pres", "ref", "raw13c", 
+               "raw18o", "err13c", "err18o", "blank", "pdbcorr13c", 
+               "pdbcorr18o", "craigcor13c", "craigcor18o")
+col_types <- c("numeric",  "numeric",  "numeric",  "text",  "numeric",
+               "text",  "numeric",  "numeric",  "numeric",  "numeric",  
+               "numeric",  "numeric",  "numeric",  "numeric",  "numeric")  
+
+read_xlsx(file, range = cell_cols("A:O"),
+          col_names = col_names, col_types = col_types) %>%
+  filter(grepl("^\\d{5}", date)) %>%
+  mutate(date = excel_numeric_to_date(as.numeric(date)),
+         time = format(as.POSIXct(Sys.Date() + as.numeric(time)), "%H:%M", tz="UTC")) %>%
+  select(-blank)
+  
+}
          
+data <- readIRMSfile(files[1])
